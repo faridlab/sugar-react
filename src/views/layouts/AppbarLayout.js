b5616ec12d52083c05a618396045e8c35aeb127b
Farid Hidayat <e.faridhidayat@gmail.com>
2020-11-15 00:18:04 +0700

    Appbarlayout

--- src/views/layouts/AppbarLayout.js
+++ src/views/layouts/AppbarLayout.js
@@ -80,7 +80,7 @@ const useStyles = makeStyles((theme) => ({
     transition: theme.transitions.create('width'),
     width: '100%',
     [theme.breakpoints.up('md')]: {
-      width: '20ch',
+      width: '30ch',
     },
   },
   sectionDesktop: {
