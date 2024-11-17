97828e63be53807398c48eb4101b4dd1dc126984
Farid Hidayat <farid@startapp.id>
2024-11-18 00:39:29 +0700

    index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import './index.css';
+import { StyledEngineProvider } from '@mui/material/styles';
+import DashboardLayout from './shared/layouts/DashboardLayout';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +10,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <StyledEngineProvider injectFirst>
+      <DashboardLayout />
+    </StyledEngineProvider>
   </React.StrictMode>
 );
 
