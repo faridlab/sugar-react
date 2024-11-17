c5e8c2e9cb113780ac7f2f7030e9554f4c94588a
Farid Hidayat <farid@startapp.id>
2024-11-18 00:56:49 +0700

    feat: Dashboard layout

---
+++ src/shared/layouts/DashboardLayout.tsx
@@ -0,0 +1,597 @@
+import * as React from 'react';
+import { styled, useTheme, Theme, CSSObject } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MuiDrawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+
+import ListItem from '@mui/material/ListItem';
+import ListItemButton, { listItemButtonClasses } from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import { red } from '@mui/material/colors';
+import { Card, CardHeader, Avatar, Menu, Switch, Alert, AlertTitle } from '@mui/material';
+
+
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import MenuItem from '@mui/material/MenuItem';
+import ListSubheader from '@mui/material/ListSubheader';
+import Select, { SelectChangeEvent, selectClasses } from '@mui/material/Select';
+import AddRoundedIcon from '@mui/icons-material/AddRounded';
+import DevicesRoundedIcon from '@mui/icons-material/DevicesRounded';
+import SmartphoneRoundedIcon from '@mui/icons-material/SmartphoneRounded';
+import ConstructionRoundedIcon from '@mui/icons-material/ConstructionRounded';
+
+import { paperClasses } from '@mui/material/Paper';
+import { listClasses } from '@mui/material/List';
+import { listItemIconClasses } from '@mui/material/ListItemIcon';
+import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
+import MoreVertRoundedIcon from '@mui/icons-material/MoreVertRounded';
+import { dividerClasses } from '@mui/material/Divider';
+
+import logostartapp from '../../assets/images/startapp-dark.png'
+
+import Collapse from '@mui/material/Collapse';
+import DraftsIcon from '@mui/icons-material/Drafts';
+import SendIcon from '@mui/icons-material/Send';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import StarBorder from '@mui/icons-material/StarBorder';
+
+import Badge, { badgeClasses } from '@mui/material/Badge';
+import { IconButtonProps } from '@mui/material/IconButton';
+
+import PageContainer from './PageContainer'
+
+const drawerWidth = 240;
+
+const openedMixin = (theme: Theme): CSSObject => ({
+  width: drawerWidth,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-start',
+  padding: 0,
+  margin: 0,
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+const DrawerHeaderSpace = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  // padding: 0,
+  // margin: 0,
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme }) => ({
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+    boxSizing: 'border-box',
+    variants: [
+      {
+        props: ({ open }) => open,
+        style: {
+          ...openedMixin(theme),
+          '& .MuiDrawer-paper': openedMixin(theme),
+        },
+      },
+      {
+        props: ({ open }) => !open,
+        style: {
+          ...closedMixin(theme),
+          '& .MuiDrawer-paper': closedMixin(theme),
+        },
+      },
+    ],
+  }),
+);
+
+
+const AvatarContent = styled(Avatar)(({ theme }) => ({
+  width: 28,
+  height: 28,
+  backgroundColor: theme.palette.background.paper,
+  color: theme.palette.text.secondary,
+  border: `1px solid ${theme.palette.divider}`,
+}));
+
+const ListItemAvatarContent = styled(ListItemAvatar)({
+  minWidth: 0,
+  marginRight: 12,
+});
+
+export function SelectContent() {
+  const [company, setCompany] = React.useState('');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setCompany(event.target.value as string);
+  };
+
+  return (
+    <Select
+      labelId="company-select"
+      id="company-simple-select"
+      value={company}
+      onChange={handleChange}
+      displayEmpty
+      inputProps={{ 'aria-label': 'Select company' }}
+      fullWidth
+      sx={{
+        maxHeight: 56,
+        width: 215,
+        '&.MuiList-root': {
+          p: '8px',
+        },
+        [`& .${selectClasses.select}`]: {
+          display: 'flex',
+          alignItems: 'center',
+          gap: '2px',
+          pl: 1,
+        },
+      }}
+    >
+      <ListSubheader sx={{ pt: 0 }}>Production</ListSubheader>
+      <MenuItem value="">
+        <ListItemAvatarContent>
+          <AvatarContent alt="Sitemark web">
+            <DevicesRoundedIcon sx={{ fontSize: '1rem' }} />
+          </AvatarContent>
+        </ListItemAvatarContent>
+        <ListItemText primary="Sitemark-web" secondary="Web app" />
+      </MenuItem>
+      <MenuItem value={10}>
+        <ListItemAvatarContent>
+          <AvatarContent alt="Sitemark App">
+            <SmartphoneRoundedIcon sx={{ fontSize: '1rem' }} />
+          </AvatarContent>
+        </ListItemAvatarContent>
+        <ListItemText primary="Sitemark-app" secondary="Mobile application" />
+      </MenuItem>
+      <MenuItem value={20}>
+        <ListItemAvatarContent>
+          <AvatarContent alt="Sitemark Store">
+            <DevicesRoundedIcon sx={{ fontSize: '1rem' }} />
+          </AvatarContent>
+        </ListItemAvatarContent>
+        <ListItemText primary="Sitemark-Store" secondary="Web app" />
+      </MenuItem>
+      <ListSubheader>Development</ListSubheader>
+      <MenuItem value={30}>
+        <ListItemAvatarContent>
+          <AvatarContent alt="Sitemark Store">
+            <ConstructionRoundedIcon sx={{ fontSize: '1rem' }} />
+          </AvatarContent>
+        </ListItemAvatarContent>
+        <ListItemText primary="Sitemark-Admin" secondary="Web app" />
+      </MenuItem>
+      <Divider sx={{ mx: -1 }} />
+      <MenuItem value={40}>
+        <ListItemIcon>
+          <AddRoundedIcon />
+        </ListItemIcon>
+        <ListItemText primary="Add product" secondary="Web app" />
+      </MenuItem>
+    </Select>
+  );
+}
+
+export interface MenuButtonProps extends IconButtonProps {
+  showBadge?: boolean;
+}
+
+export function MenuButton({
+  showBadge = false,
+  ...props
+}: MenuButtonProps) {
+  return (
+    <Badge
+      color="error"
+      variant="dot"
+      invisible={!showBadge}
+      sx={{ [`& .${badgeClasses.badge}`]: { right: 2, top: 2 } }}
+    >
+      <IconButton size="small" {...props} />
+    </Badge>
+  );
+}
+
+
+const MenuOptionItem = styled(MenuItem)({
+  margin: '2px 0',
+});
+
+export function OptionsMenu() {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <React.Fragment>
+      <MenuButton
+        aria-label="Open menu"
+        onClick={handleClick}
+        sx={{ borderColor: 'transparent' }}
+      >
+        <MoreVertRoundedIcon />
+      </MenuButton>
+      <Menu
+        anchorEl={anchorEl}
+        id="menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+        sx={{
+          [`& .${listClasses.root}`]: {
+            padding: '4px',
+          },
+          [`& .${paperClasses.root}`]: {
+            padding: 0,
+          },
+          [`& .${dividerClasses.root}`]: {
+            margin: '4px -4px',
+          },
+        }}
+      >
+        <MenuOptionItem onClick={handleClose}>Profile</MenuOptionItem>
+        <MenuOptionItem onClick={handleClose}>My account</MenuOptionItem>
+        <Divider />
+        <MenuOptionItem onClick={handleClose}>Add another account</MenuOptionItem>
+        <MenuOptionItem onClick={handleClose}>Settings</MenuOptionItem>
+        <Divider />
+        <MenuOptionItem
+          onClick={handleClose}
+          sx={{
+            [`& .${listItemIconClasses.root}`]: {
+              ml: 'auto',
+              minWidth: 0,
+            },
+          }}
+        >
+          <ListItemText>Logout</ListItemText>
+          <ListItemIcon>
+            <LogoutRoundedIcon fontSize="small" />
+          </ListItemIcon>
+        </MenuOptionItem>
+      </Menu>
+    </React.Fragment>
+  );
+}
+
+
+const DarkModeSwitcher = styled(Switch)(({ theme }) => ({
+  // width: 62,
+  height: 34,
+  // padding: 7,
+  '& .MuiSwitch-switchBase': {
+    margin: 1,
+    padding: 0,
+    transform: 'translateX(6px)',
+    '&.Mui-checked': {
+      color: '#fff',
+      transform: 'translateX(22px)',
+      '& .MuiSwitch-thumb:before': {
+        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+          '#fff',
+        )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
+      },
+      '& + .MuiSwitch-track': {
+        opacity: 1,
+        backgroundColor: '#aab4be',
+        ...theme.applyStyles('dark', {
+          backgroundColor: '#8796A5',
+        }),
+      },
+    },
+  },
+  '& .MuiSwitch-thumb': {
+    backgroundColor: '#001e3c',
+    width: 32,
+    height: 32,
+    '&::before': {
+      content: "''",
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      left: 0,
+      top: 0,
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+        '#fff',
+      )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
+    },
+    ...theme.applyStyles('dark', {
+      backgroundColor: '#003892',
+    }),
+  },
+  '& .MuiSwitch-track': {
+    opacity: 1,
+    backgroundColor: '#aab4be',
+    borderRadius: 20 / 2,
+    ...theme.applyStyles('dark', {
+      backgroundColor: '#8796A5',
+    }),
+  },
+}));
+
+export default function DashboardLayout() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(true);
+  const [openTree, setOpenTree] = React.useState(false);
+  const [openNav, setOpenNav] = React.useState(true);
+
+  const handleClick = () => {
+    setOpenTree(!openTree);
+  };
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <Drawer variant="permanent" open={open}>
+        {/* <DrawerHeader>
+        <Card variant='outlined' sx={{ border: 0, borderRadius: 0, width: '100%' }}>
+            <CardHeader
+                avatar={
+                    <Avatar sx={{ bgcolor: red[500] }} aria-label="Startapp">ST</Avatar>
+                }
+                action={
+                    <IconButton aria-label="more">
+                        <MoreVertIcon color='inherit' />
+                    </IconButton>
+                }
+                title="Startapp"
+                subheader="Pesawat Kertas"
+            />
+        </Card>
+        </DrawerHeader> */}
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', p: 2 }}>
+          <img src={logostartapp} alt="Startapp logo" style={{ height: '36px', marginTop: '-6px', marginBottom: '-6px' }} />
+          <DarkModeSwitcher sx={{ marginTop: '-5px', marginBottom: '-5px' }} defaultChecked />
+        </Box>
+        <Divider />
+        <DrawerHeaderSpace>
+          <SelectContent />
+        </DrawerHeaderSpace>
+        <Box
+          sx={{
+            minHeight: 0,
+            overflow: 'hidden auto',
+            flexGrow: 1,
+            display: 'flex',
+            flexDirection: 'column',
+            [`& .${listItemButtonClasses.root}`]: {
+              gap: 1.5,
+            },
+          }}
+        >
+
+        <List
+          sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+          component="nav"
+          aria-labelledby="nested-list-subheader"
+          subheader={
+            <ListSubheader component="div" id="nested-list-subheader">
+              Nested List Items
+            </ListSubheader>
+          }
+        >
+          <ListItemButton>
+            <ListItemIcon>
+              <SendIcon />
+            </ListItemIcon>
+            <ListItemText primary="Sent mail" />
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemIcon>
+              <DraftsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Drafts" />
+          </ListItemButton>
+          <ListItemButton onClick={handleClick}>
+            <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary="Inbox" />
+            {openTree ? <ExpandLess /> : <ExpandMore />}
+          </ListItemButton>
+          <Collapse in={openTree} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding dense >
+              <ListItemButton sx={{ pl: 4 }}>
+                <ListItemIcon>
+                  <StarBorder />
+                </ListItemIcon>
+                <ListItemText primary="Starred" />
+              </ListItemButton>
+              <ListItemButton sx={{ pl: 4 }}>
+                <ListItemIcon>
+                  <StarBorder />
+                </ListItemIcon>
+                <ListItemText primary="Starred" />
+              </ListItemButton>
+              <ListItemButton sx={{ pl: 4 }}>
+                <ListItemIcon>
+                  <StarBorder />
+                </ListItemIcon>
+                <ListItemText primary="Starred" />
+              </ListItemButton>
+              <ListItemButton sx={{ pl: 4 }}>
+                <ListItemIcon>
+                  <StarBorder />
+                </ListItemIcon>
+                <ListItemText primary="Starred" />
+              </ListItemButton>
+            </List>
+          </Collapse>
+        </List>
+        <Divider />
+
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem key={text} disablePadding sx={{ display: 'block' }}>
+              <ListItemButton
+                sx={[
+                  {
+                    minHeight: 48,
+                    px: 2.5,
+                  },
+                  open
+                    ? {
+                        justifyContent: 'initial',
+                      }
+                    : {
+                        justifyContent: 'center',
+                      },
+                ]}
+              >
+                <ListItemIcon
+                  sx={[
+                    {
+                      minWidth: 0,
+                      justifyContent: 'center',
+                    },
+                    open
+                      ? {
+                          mr: 3,
+                        }
+                      : {
+                          mr: 'auto',
+                        },
+                  ]}
+                >
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText
+                  primary={text}
+                  sx={[
+                    open
+                      ? {
+                          opacity: 1,
+                        }
+                      : {
+                          opacity: 0,
+                        },
+                  ]}
+                />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem key={text} disablePadding sx={{ display: 'block' }}>
+              <ListItemButton
+                sx={[
+                  {
+                    minHeight: 48,
+                    px: 2.5,
+                  },
+                  open
+                    ? {
+                        justifyContent: 'initial',
+                      }
+                    : {
+                        justifyContent: 'center',
+                      },
+                ]}
+              >
+                <ListItemIcon
+                  sx={[
+                    {
+                      minWidth: 0,
+                      justifyContent: 'center',
+                    },
+                    open
+                      ? {
+                          mr: 3,
+                        }
+                      : {
+                          mr: 'auto',
+                        },
+                  ]}
+                >
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText
+                  primary={text}
+                  sx={[
+                    open
+                      ? {
+                          opacity: 1,
+                        }
+                      : {
+                          opacity: 0,
+                        },
+                  ]}
+                />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+
+        <Alert severity="success">
+          <AlertTitle>Success</AlertTitle>
+            This is a success Alert with an encouraging title.
+        </Alert>
+        </Box>
+        <Divider />
+        <Card sx={{ borderRadius: 0 }}>
+            <CardHeader
+                avatar={
+                    <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">FH</Avatar>
+                }
+                action={
+                  <OptionsMenu />
+                }
+                title="Farid hidayat"
+                subheader="Superadmin"
+            />
+        </Card>
+      </Drawer>
+      <PageContainer />
+    </Box>
+  );
+}
