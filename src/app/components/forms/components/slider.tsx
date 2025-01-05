39fa0d606bb73dec9e395f4c51bb730030af9229
Farid Hidayat <farid@startapp.id>
2025-01-05 23:49:09 +0700

    feat: form slider

---
+++ src/app/components/forms/components/slider.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react'
+import { FunctionComponent, useContext } from 'react'
+import FormControl from '@mui/material/FormControl'
+import FormLabel from '@mui/material/FormLabel'
+import Slider from '@mui/material/Slider'
+import { FormProps, FormContext } from '@component/forms'
+
+const FormSlider: FunctionComponent<FormProps> = (formProps: FormProps) => {
+  const { data, setData } = useContext(FormContext)
+  const { id, label, props } = formProps
+  const properties = {
+    fullWidth: true,
+    type: 'text',
+    ...props,
+  }
+
+  const handleChange = (_event: Event, value: number | number[]) => {
+    setData({ ...data, [id]: value })
+  }
+
+  return (
+    <FormControl component="fieldset">
+      <FormLabel component="legend">{label}</FormLabel>
+      <Slider
+        {...properties}
+        name={id}
+        value={data[id]}
+        onChange={handleChange}
+      />
+    </FormControl>
+  )
+}
+
+export default FormSlider
