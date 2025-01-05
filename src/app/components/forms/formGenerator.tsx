cc38b97f8d593c4c9c82a07d5873b32e8d2a45e3
Farid Hidayat <farid@startapp.id>
2025-01-05 23:49:45 +0700

    feat: form generator and components

---
+++ src/app/components/forms/formGenerator.tsx
@@ -0,0 +1,86 @@
+import * as React from 'react'
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+import { FormLayoutProps, FormProps, FormContext } from '@component/forms'
+import { FunctionComponent, useEffect, useState } from 'react'
+import * as FormComponents from '@component/forms/components'
+
+export interface FormGeneratorProps {
+  forms: FormLayoutProps;
+  data: Record<string, any>;
+  onDataChanged: Function;
+  onSubmit?: Function;
+  readOnly?: boolean;
+}
+
+type LayoutProps = Pick<FormGeneratorProps, 'forms' | 'readOnly'>
+
+// Add this type definition
+type FormComponentsType = {
+  [key: string]: React.ComponentType<FormProps>;
+};
+
+const FormInputGenerator: FunctionComponent<FormProps> = (props: FormProps) => {
+  const { type } = props
+  const FormComponent = (FormComponents as FormComponentsType)[type]
+  return <FormComponent {...props} />
+}
+
+const FormLayoutGenerator: FunctionComponent<LayoutProps> = (props: LayoutProps) => {
+  const { forms } = props
+  const layout = []
+  for (const index in forms) {
+    const items = forms[index]
+    const fields = []
+    for (const props of items) {
+      const { col } = props
+      fields.push(<Grid key={`field-${props.id}`} item xs={col} sx={{mb: 4}}>
+        <FormInputGenerator {...props} />
+      </Grid>)
+    }
+    layout.push(<Grid key={index} container spacing={2}>{fields}</Grid>)
+  }
+  return (<>{layout}</>)
+}
+
+const FormGenerator: FunctionComponent<FormGeneratorProps> = ({readOnly = false, ...props}: FormGeneratorProps) => {
+  const { forms, onDataChanged, onSubmit } = props
+  const [ data, setData ] = useState(props.data)
+  useEffect(() => {
+    setData(props.data)
+  }, [props.data])
+
+  // TODO: find another way to set readonly props to input
+  for (const items of forms) {
+    for (const form of items) {
+      form.props['disabled'] = readOnly
+    }
+  }
+
+  useEffect(() => {
+    onDataChanged(data)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data])
+
+  if (Object.keys(data).length === 0) return <></>
+  return (
+    <FormContext.Provider value={{data, setData}}>
+      <Box
+        id='form-generator'
+        component="form"
+        onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+          e.preventDefault();
+          if (onSubmit) onSubmit(e);
+        }}
+        sx={{ flexGrow: 1, m: 2 }}
+      >
+        <FormLayoutGenerator
+          forms={forms}
+          readOnly={readOnly}
+        />
+      </Box>
+    </FormContext.Provider>
+  )
+}
+
+export default FormGenerator
---
+++ src/app/components/forms/index.tsx
@@ -0,0 +1,42 @@
+import { FunctionComponent } from "react"
+import FormGenerator from '@component/forms/formGenerator'
+import TextField from '@component/forms/components/textField'
+import Checkbox from '@component/forms/components/checkbox'
+import Radio from '@component/forms/components/radio'
+import Rating from '@component/forms/components/rating'
+import Switch from '@component/forms/components/switch'
+import Slider from '@component/forms/components/slider'
+import Select from '@component/forms/components/select'
+import FormContext from '@component/forms/contexts'
+
+export interface FormPropsComponent {
+  create?: FunctionComponent;
+  show?: FunctionComponent;
+  edit?: FunctionComponent;
+}
+
+export interface FormProps {
+  id: string;
+  col?: number;
+  required?: boolean;
+  label?: string;
+  type: string;
+  component?: FunctionComponent<FormProps> | FormPropsComponent // FIXME: this type arise error on react run time, should i remove this type?
+  props: Record<string, any>, // used for dynamic props component
+  reference?: Record<string, any>,
+}
+
+export type FormLayoutProps = Array<FormProps[]>
+
+export default FormGenerator
+export {
+  FormContext,
+
+  TextField,
+  Checkbox,
+  Radio,
+  Rating,
+  Switch,
+  Slider,
+  Select
+}
