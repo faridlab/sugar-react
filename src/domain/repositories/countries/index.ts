6f948bc7cd55526e7c8fa320fe8f7ccda1bd7d98
Farid Hidayat <farid@startapp.id>
2025-01-07 23:27:57 +0700

    feat(repo): countries

---
+++ src/domain/repositories/countries/index.ts
@@ -0,0 +1,9 @@
+import data from '@domain/repositories/countries/data'
+import forms from '@domain/repositories/countries/forms'
+import validation from '@domain/repositories/countries/validation'
+
+export {
+  data,
+  forms,
+  validation
+}
