c016449b69d9dbb532901e8ac56ce0587877cc6b
Farid Hidayat <farid@startapp.id>
2025-01-07 22:43:02 +0700

    feat(component): layout DashboardLayout

---
+++ src/app/layouts/dashboard.tsx
@@ -0,0 +1,122 @@
+// import type { NextPage } from 'next'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import SidemenuComponent from './sidemenu'
+import TopnavComponent from './topnavbar'
+
+import Drawer from '@mui/material/Drawer';
+import { FunctionComponent, PropsWithChildren } from 'react';
+import useFilterParams from '@app/hooks/useFilterParams'
+import FilterToolbar from '@component/layouts/filterToolbar';
+interface FilterParams extends PropsWithChildren {
+  filterParams?: ReturnType<typeof useFilterParams>;
+  title?: string;
+  ToolbarActions?: FunctionComponent;
+}
+
+const drawerWidth = 280;
+
+const DashboardLayout = ({ children, filterParams, title, ToolbarActions }: FilterParams) => {
+  const [ isOpenDrawer, setOpenDrawer ] = React.useState<boolean>(true)
+  const toggleOpenDrawer = () => {
+    setOpenDrawer(!isOpenDrawer)
+  }
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <TopnavComponent
+        title={title}
+        onToggleDrawer={toggleOpenDrawer}
+      />
+
+      <Drawer
+        variant="persistent"
+        sx={{
+          width: isOpenDrawer? drawerWidth: 0,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+        }}
+        open={isOpenDrawer}
+      >
+        <Toolbar />
+        <Box sx={{ overflow: 'auto' }}>
+          <SidemenuComponent />
+        </Box>
+      </Drawer>
+
+      <Box
+        sx={{
+          flexGrow: 1,
+          backgroundColor: (theme) =>
+            theme.palette.mode === 'light'
+              ? theme.palette.grey[100]
+              : theme.palette.grey[900],
+        }}>
+
+        <FilterToolbar
+          filterParams={filterParams}
+          ToolbarActions={ToolbarActions}
+        />
+
+        <Box
+          component="main"
+          sx={{
+            paddingX: 2,
+            paddingBottom: 6
+          }}
+        >
+          {children}
+        </Box>
+
+      </Box>
+
+      {/*
+      <Drawer
+        variant="permanent"
+        anchor='right'
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+        }}
+      >
+        <Toolbar />
+        <Box sx={{ overflow: 'auto' }}>
+          <List>
+            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {['All mail', 'Trash', 'Spam'].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Drawer>
+      */}
+
+    </Box>
+  )
+}
+
+export default DashboardLayout
