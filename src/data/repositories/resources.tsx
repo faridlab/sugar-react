5259a3ef232100c77198d4fb3d90c8a52b14ed07
Farid Hidayat <farid@startapp.id>
2025-01-07 22:51:54 +0700

    feat(repo): resources

---
+++ src/data/repositories/resources.tsx
@@ -0,0 +1,15 @@
+import {
+  data,
+  // columns, NOTE: because columns needs type from react-data-grid, it's not possible to use import from domain context
+  forms,
+  validation
+} from '@domain/repositories/resources'
+
+const params = {}
+
+export {
+  data,
+  forms,
+  validation,
+  params
+}
