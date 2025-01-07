bc8c1fc283e89c225b0b9e1eb331633614271e85
Farid Hidayat <farid@startapp.id>
2025-01-07 22:53:44 +0700

    feat(repo): permissions

---
+++ src/data/repositories/permissions/columns.tsx
@@ -0,0 +1,19 @@
+import { GridColDef } from "@mui/x-data-grid"
+
+const columns: GridColDef[] = [
+  {
+    field: 'name',
+    headerName: 'Permission',
+    editable: false,
+    width: 160
+  },
+  {
+    field: 'guard_name',
+    headerName: 'Guard',
+    editable: false,
+    width: 200,
+    flex: 1,
+  },
+]
+
+export default columns
