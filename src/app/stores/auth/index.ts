aae66a62fab07136f47c8bd0ca0ecb5ec8c41352
Farid Hidayat <farid@startapp.id>
2025-01-07 22:49:16 +0700

    feat: import/export store

---
+++ src/app/stores/auth/index.ts
@@ -0,0 +1,24 @@
+import resourceReducer,
+{
+  setUser,
+  setToken,
+  checkToken,
+  removeUser,
+  removeToken,
+  userLogout
+} from "./reducers"
+
+export {
+  selectResource
+} from './selectors'
+
+export {
+  setUser,
+  setToken,
+  checkToken,
+  removeUser,
+  removeToken,
+  userLogout
+}
+
+export default resourceReducer
