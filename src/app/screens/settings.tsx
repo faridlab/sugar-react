c6bb6bc87a92f67f9ca962a900f24aabfeb04615
Farid Hidayat <farid@startapp.id>
2025-01-12 15:26:41 +0700

    feat(screen): settings

---
+++ src/app/screens/settings.tsx
@@ -0,0 +1,20 @@
+
+const SettingsPage = () => {
+  return (
+    <>
+      {/* <Head>
+        <title>Setting</title>
+        <meta name="description" content="New Collection" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head> */}
+
+      <main>
+        <h1>
+          Setting Page
+        </h1>
+      </main>
+    </>
+  )
+}
+
+export default SettingsPage
