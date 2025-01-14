57310afaa8009a61c943b9d0de0c7becbb64836c
Farid Hidayat <farid@startapp.id>
2025-01-14 14:53:12 +0700

    refactor(store): redux store

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,20 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router";
 import reportWebVitals from './reportWebVitals';
 import Routers from './app/routes';
+import { Provider } from 'react-redux'
+import { store } from './app/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <Provider store={store}>
   <BrowserRouter>
     <React.StrictMode>
       <Routers />
     </React.StrictMode>
   </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
