5f5235b40bcf0ff093bd2ee922b64232b8b66f57
Farid Hidayat <farid@startapp.id>
2025-01-22 23:28:19 +0700

    fix: update API endpoint URLs for user login

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,2 +1,2 @@
-export const ENDPOINT_API_URL = 'http://localhost:8080/api/v1'
-export const API_USER_LOGIN = '/api/auth/login'
+export const ENDPOINT_API_URL = 'http://localhost:8000/api/v1'
+export const API_USER_LOGIN = '/user/login'
