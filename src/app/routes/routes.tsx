1bcf7630e10c85cc546ebd02c1a95ba2ce9c7fc6
Farid Hidayat <farid@startapp.id>
2025-02-03 00:17:42 +0700

    feat: route authenticate secreen

--- src/app/routes/routes.tsx
+++ src/app/routes/routes.tsx
@@ -2,22 +2,48 @@ import { createBrowserRouter } from 'react-router';
 import SignInScreen, {LoaderData} from "../screens/SignInScreen";
 import SignUpScreen from "../screens/SignUpScreen";
 import DashboardScreen from '../screens/DashboardScreen';
+import AuthenticatedRoutes from '../presenters/auth/Authenticated';
+import AuthenticatedToDashboard from '../presenters/auth/AuthenticatedToDashboard';
+import ResourcesScreen from '../screens/resources';
+import DashboardLayout from '../layouts/DashboardLayout';
 
 const routers = createBrowserRouter([
   {
-    path: "/",
-    index: true,
-    element: <DashboardScreen />,
+    element: <AuthenticatedToDashboard />,
+    children: [
+      {
+        path: "/sign-in",
+        element: <SignInScreen />,
+        loader: LoaderData,
+      },
+      {
+        path: "/sign-up",
+        element: <SignUpScreen />,
+      },
+    ]
   },
+
+  // Authenticated routes
   {
-    path: "/sign-in",
-    element: <SignInScreen />,
-    loader: LoaderData,
-  },
-  {
-    path: "/sign-up",
-    element: <SignUpScreen />,
-  },
+    element: <AuthenticatedRoutes />,
+    children: [
+      {
+        element: <DashboardLayout />,
+        children: [
+          {
+            path: "/",
+            index: true,
+            element: <DashboardScreen />,
+          },
+          {
+            path: "/:collection",
+            element: <ResourcesScreen />,
+            loader: LoaderData,
+          },
+        ]
+      },
+    ]
+  }
 ]);
 
 export default routers
