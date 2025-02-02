dacf64e6c8a40fded68a3b9433f5edefa68d28d5
Farid Hidayat <farid@startapp.id>
2025-02-03 00:15:32 +0700

    feat: DashboardLayout

--- src/app/layouts/DashboardLayout.tsx
+++ src/app/layouts/DashboardLayout.tsx
@@ -2,15 +2,16 @@ import Box from '@mui/material/Box'
 import CssBaseline from '@mui/material/CssBaseline'
 import PageContainer from '../presenters/dashboard/PageContainer'
 import DrawerMenu from '../presenters/dashboard/DrawerMenu'
+import { Outlet } from 'react-router'
 
 export default function DashboardLayout(props: React.PropsWithChildren)  {
-  const { children } = props
+  // const { children } = props
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
       <DrawerMenu />
       <PageContainer>
-        {children}
+        <Outlet />
       </PageContainer>
     </Box>
   )
