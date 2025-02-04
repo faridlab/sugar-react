163b5f72379275e792b9b9f70d2fc2c2bd4899d2
Farid Hidayat <farid@startapp.id>
2025-02-04 17:21:49 +0700

    feat: ResourceContext

--- src/app/layouts/DashboardLayout.tsx
+++ src/app/layouts/DashboardLayout.tsx
@@ -1,12 +1,21 @@
 import Box from '@mui/material/Box'
 import CssBaseline from '@mui/material/CssBaseline'
-import PageContainer from '../presenters/dashboard/PageContainer'
+import PageContainer, { BreadcrumbLink } from '../presenters/dashboard/PageContainer'
 import DrawerMenu from '../presenters/dashboard/DrawerMenu'
 import { Outlet } from 'react-router'
+import { ResourceContext } from '../contexts'
+import { useState } from 'react'
+
+interface ResourceContextData {
+  ctx?: BreadcrumbLink[];
+  pageTitle?: string;
+}
 
 export default function DashboardLayout(props: React.PropsWithChildren)  {
   // const { children } = props
+  const [ ctx, setContext ] = useState<ResourceContextData>({})
   return (
+    <ResourceContext.Provider value={{ctx, setContext}}>
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
       <DrawerMenu />
@@ -14,5 +23,6 @@ export default function DashboardLayout(props: React.PropsWithChildren)  {
         <Outlet />
       </PageContainer>
     </Box>
+    </ResourceContext.Provider>
   )
 }
