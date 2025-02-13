4f1fa6341e1da87c67edb7604f1381e54961e45b
Farid Hidayat <farid@startapp.id>
2025-02-14 00:03:00 +0700

    faet: route generator

--- src/app/routes/routes.tsx
+++ src/app/routes/routes.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from 'react-router';
+import { createBrowserRouter, RouteObject } from 'react-router';
 import SignInScreen, {LoaderData} from "../screens/SignInScreen";
 import SignUpScreen from "../screens/SignUpScreen";
 import DashboardScreen from '../screens/DashboardScreen';
@@ -6,6 +6,24 @@ import AuthenticatedRoutes from '../presenters/auth/Authenticated';
 import AuthenticatedToDashboard from '../presenters/auth/AuthenticatedToDashboard';
 import ResourcesIndexScreen, { ResourcesDetailScreen } from '../screens/resources';
 import DashboardLayout from '../layouts/DashboardLayout';
+
+export function generateRoutes(collection: string): RouteObject[] {
+  const routes: RouteObject[]  = [
+    {
+        path: `/${collection}`,
+        element: <ResourcesIndexScreen storeCollection={collection} />,
+    },
+    {
+        path: `/${collection}/create`,
+        element: <ResourcesDetailScreen storeCollection={collection} />,
+    },
+    {
+        path: `/${collection}/:id`,
+        element: <ResourcesDetailScreen storeCollection={collection} />,
+    },
+  ]
+  return routes
+}
 
 const routers = createBrowserRouter([
   {
@@ -35,6 +53,11 @@ const routers = createBrowserRouter([
             index: true,
             element: <DashboardScreen />,
           },
+
+          ...generateRoutes('countries'),
+          ...generateRoutes('provinces'),
+          ...generateRoutes('cities'),
+
           {
             path: "/:collection",
             element: <ResourcesIndexScreen />,
